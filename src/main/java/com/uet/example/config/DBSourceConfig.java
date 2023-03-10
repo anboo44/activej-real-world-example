fec4672a90e0ece0f3045591d28cbb24a4ef1539
hung_pt <hung_pt@flinters.vn>
2023-03-10 13:13:35 +0700

    add manual config

--- src/main/java/com/uet/example/config/DBSourceConfig.java
+++ src/main/java/com/uet/example/config/DBSourceConfig.java
@@ -15,18 +15,24 @@ public class DBSourceConfig {
     public SessionFactory makeSessionFactory() {
         // Hibernate core config
         Configuration configuration = new Configuration();
-        configuration.setProperty("hibernate.connection.provider_class", HikariCPConnectionProvider.class.getName());
         configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-        configuration.setProperty("hibernate.dialect2", "org.hibernate.dialect.MySQLDialect");
-        configuration.setProperty("hibernate.show_sql", "false");
-        configuration.setProperty("org.hibernate.SQL_SLOW", "info");
+
+        // No use hikari
+//        configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+//        configuration.setProperty("hibernate.connection.url", configLoader.db.url);
+//        configuration.setProperty("hibernate.connection.username", configLoader.db.username);
+//        configuration.setProperty("hibernate.connection.password", configLoader.db.password);
+//        configuration.setProperty("hibernate.show_sql", "false");
+//        configuration.setProperty("org.hibernate.SQL_SLOW", "info");
 
         // Hibernate with HikariCP config
+        configuration.setProperty("hibernate.connection.provider_class", HikariCPConnectionProvider.class.getName());
         configuration.setProperty("hibernate.hikari.dataSourceClassName", "com.mysql.cj.jdbc.MysqlDataSource");
         configuration.setProperty("hibernate.hikari.dataSource.url", configLoader.db.url);
         configuration.setProperty("hibernate.hikari.dataSource.user", configLoader.db.username);
         configuration.setProperty("hibernate.hikari.dataSource.password", configLoader.db.password);
         configuration.setProperty("hibernate.hikari.maximumPoolSize", "10");
+        configuration.setProperty("hibernate.hikari.minimumIdle", "10");
 
         // HikariCP config is only for MySQL
         // https://github.com/brettwooldridge/HikariCP/wiki/MySQL-Configuration
